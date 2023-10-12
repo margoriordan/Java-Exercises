2de6b21d042482ff626ab18185d3b8b36fd0651b
margoriordan <margaret.rrdn@gmail.com>
2023-10-12 11:59:02 -0400

    add lab 7

---
+++ Lab7/src/edu/cscc/ElectricCar.java
@@ -0,0 +1,21 @@
+package edu.cscc;
+
+// Margaret Riordan | 12 Oct 2023 | Lab 7: Inheritance and Polymorphism using Cars
+public class ElectricCar extends Car{
+    int batterySize;
+
+    ElectricCar(String make, String model, int year, int batterySize){
+        super(make, model, year);
+        this.batterySize = batterySize;
+    }
+    public void setBatterySize(){this.batterySize = batterySize;}
+    public int getBatterySize(){return batterySize;}
+
+    public void describe(){
+        System.out.println("Make: "+this.getMake()+
+                "\nModel: "+this.getModel()+
+                "\nYear: "+this.getYear()+
+                "\nBattery Size: "+this.batterySize);
+    }
+
+}
---
+++ Lab7/src/edu/cscc/GasolineCar.java
@@ -0,0 +1,30 @@
+package edu.cscc;
+
+// Margaret Riordan | 12 Oct 2023 | Lab 7: Inheritance and Polymorphism using Cars
+public class GasolineCar extends Car {
+    private int cylinders;
+    private double tankCapacity;
+
+    GasolineCar(String make, String model, int year ,int cylinders, double tankCapacity){
+        super(make, model, year);
+        this.cylinders = cylinders;
+        this.tankCapacity = tankCapacity;
+    }
+
+    public int getCylinders() {return cylinders;}
+    public double getTankCapacity(){return tankCapacity;}
+
+    public void setCylinders(){this.cylinders = cylinders;}
+
+    public void  setTankCapacity(){this.tankCapacity = tankCapacity;}
+
+    public void describe(){
+        System.out.println("Make: "+this.getMake()+
+                "\nModel: "+this.getModel()+
+                "\nYear: "+this.getYear()+
+                "\nCylinders: "+this.cylinders+
+                "\nTank Capacity: "+this.tankCapacity);
+
+    }
+
+}
